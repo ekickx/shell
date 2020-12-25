1bb0c435a3694087cf1920942efc0a036941ef85
Ghani Rafif <ghani.rafif.2112@gmail.com>
2020-12-25 15:18:07 +0700

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # Enable expansion
 shopt -s extglob
 
-SOURCE_DIR="$(dirname $0)"
+SOURCE_DIR=$(realpath "$(dirname $0)")
 EXCLUDE=("deploy.sh" "README.md")
 
 echo "These files will be deployed to:" 
 echo -e "(Backup your files first before doing this operation)\n"
 for f in $(ls $SOURCE_DIR); do
   if [[ ! "${EXCLUDE[@]}" =~ "$f" ]]; then
-    echo "$f" -> ~/."$f"
+    echo "$f -> ~/.$f"
   fi
 done
 
@@ -21,6 +21,11 @@ if [[ $input == "y" ]]; then
   for f in $(ls $SOURCE_DIR); do
       # Only link that's not in exclude list
     if [[ ! "${EXCLUDE[@]}" =~ "$f" ]]; then
+      # Check if dir exist and it's not a symlink
+      # If yes then only link the content
+      # Else link the dir
+      [[ -d ~/."$f" && ! -L ~/."$f" ]] && \
+        ln -snf "$SOURCE_DIR"/"$f"/* "$HOME"/."$f"/ || \
         ln -snf "$SOURCE_DIR"/"$f" "$HOME"/."$f"
     fi
   done
